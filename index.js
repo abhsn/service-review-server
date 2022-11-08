dd5858d96e0dc6cd8e73c93938669591d667b3b7
Abid Hasan <ab1dhasan@outlook.com>
2022-11-08 16:30:32 +0600

    sends limited services for home page

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
 const app = express();
 const port = 5000;
+
+require('dotenv').config();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.status(200).send('Service Review server is running...');
 });
+
+const client = new MongoClient(process.env.MONGO_URI);
+
+async function crudOperation() {
+  const serviceCollection = client.db("service-review").collection("service-list");
+
+  app.get('/services', async (req, res) => {
+    const query = {};
+    const limit = 3;
+    if (req.headers.isshort) {
+      const cursor = serviceCollection.find(query).limit(limit);
+      const services = await cursor.toArray();
+      res.send(services);
+    }
+  });
+
+}
+
+crudOperation();
 
 app.listen(port, () => console.log('Listening on port', port));
